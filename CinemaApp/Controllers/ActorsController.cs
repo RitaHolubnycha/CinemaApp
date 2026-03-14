ca14dd958473d47e21608de6e92ef2a71a8bb519
Rita Holucbycha <ritaholubnytha@gmail.com>
2026-03-14 14:28:17 +0200

    Add project files.

---
+++ CinemaApp/Controllers/ActorsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CinemaApp.Data;
+using CinemaApp.Models;
+
+namespace CinemaApp.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly CinemaAppContext _context;
+
+        public ActorsController(CinemaAppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Actors
+        public async Task<IActionResult> Index()
+        {
+            var actors = await _context.Actors
+                .Include(a => a.FilmActors)
+                .ThenInclude(fa => fa.Film)
+                .ToListAsync();
+
+            return View(actors);
+        }
+
+        // GET: Actors/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var actor = await _context.Actors
+                .Include(a => a.FilmActors)
+                .ThenInclude(fa => fa.Film)
+                .FirstOrDefaultAsync(a => a.ActorId == id);
+
+            if (actor == null)
+                return NotFound();
+
+            return View(actor);
+        }
+
+        // GET: Actors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Actors/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("FirstName,LastName,BirthDate,Nationality,Biography")] Actor actor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(actor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(actor);
+        }
+
+        // GET: Actors/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var actor = await _context.Actors.FindAsync(id);
+            if (actor == null)
+                return NotFound();
+
+            return View(actor);
+        }
+
+        // POST: Actors/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ActorId,FirstName,LastName,BirthDate,Nationality,Biography")] Actor actor)
+        {
+            if (id != actor.ActorId)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(actor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Actors.Any(e => e.ActorId == actor.ActorId))
+                        return NotFound();
+                    else
+                        throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(actor);
+        }
+
+        // GET: Actors/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var actor = await _context.Actors
+                .FirstOrDefaultAsync(a => a.ActorId == id);
+
+            if (actor == null)
+                return NotFound();
+
+            return View(actor);
+        }
+
+        // POST: Actors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var actor = await _context.Actors.FindAsync(id);
+            if (actor != null)
+            {
+                _context.Actors.Remove(actor);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ CinemaApp/Models/Film.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+using System;
+using System.Collections.Generic;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace CinemaApp.Models
+{
+    public class Film
+    {
+        public int FilmId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public DateTime? ReleaseDate { get; set; }
+
+        public string? Genre { get; set; }
+
+        public string? Description { get; set; }
+
+        public ICollection<FilmActor>? FilmActors { get; set; }
+    }
+}
